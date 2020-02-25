28d806beb61bc0cc68b480df47a5ea3c4e5d5f10
Lorenzo <lorenzo.franzone@gmail.com>
2020-02-25 15:28:22 +0100

    Start

---
+++ database.php
@@ -0,0 +1,66 @@
+<?php
+
+  $database = [
+    [
+      'title' => 'Always the Hard Way',
+      'author' => 'Terror',
+      'year' => 2006,
+      'poster' => ''
+    ], // 001
+    [
+      'title' => 'Killswitch Engage',
+      'author' => 'Disarm the Discent',
+      'year' => 2013,
+      'poster' => ''
+    ], // 002
+    [
+      'title' => 'Black Flame',
+      'author' => 'Bury Tomorrow',
+      'year' => 2018,
+      'poster' => ''
+    ], // 003
+    [
+      'title' => 'The Jester Race',
+      'author' => 'In Flames',
+      'year' => 2002,
+      'poster' => ''
+    ], // 004
+    [
+      'title' => 'The Rise of Brutality',
+      'author' => 'Hatebreed',
+      'year' => 2003,
+      'poster' => ''
+    ], // 005
+    [
+      'title' => 'Reign in Blood',
+      'author' => 'Slayer',
+      'year' => 1986,
+      'poster' => ''
+    ], // 006
+    [
+      'title' => 'Peace Sells...But Who\'s Buying',
+      'author' => 'Megadeth',
+      'year' => 1986,
+      'poster' => ''
+    ], // 007
+    [
+      'title' => 'Iron Maiden',
+      'author' => 'Somewhere in Time',
+      'year' => 1986,
+      'poster' => ''
+    ], // 008
+    [
+      'title' => 'The Dissent of Man',
+      'author' => 'Bad Religion',
+      'year' => 2010,
+      'poster' => ''
+    ], // 009
+    [
+      'title' => 'Endgame',
+      'author' => 'Rise Against',
+      'year' => 2011,
+      'poster' => ''
+    ], // 010
+  ]; // $database
+
+?>
