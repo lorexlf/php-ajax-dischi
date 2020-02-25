4d5afa4852229bf308f4cf3fce8517fca6d81382
Lorenzo <lorenzo.franzone@gmail.com>
2020-02-25 16:09:27 +0100

    Base Style

--- index.php
+++ index.php
@@ -31,18 +31,19 @@
   <main class="py-5 txt-small txt-center">
     <div class="container">
       <div class="grid-row">
-        <!-- Col-1 -->
+        <!-- Loop -->
         <div class="col-sm-6 col-md-4">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div> <!-- / Col-1 -->
-        <!-- Col-2 -->
-        <div class="col-sm-6 col-md-4">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div> <!-- / Col-2 -->
-        <!-- Col-3 -->
-        <div class="col-sm-6 col-md-4">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div> <!-- / Col-3 -->
+          <div class="item p-3">
+            <div class="image">
+              <img src="https://fanart.tv/api/download.php?type=download&image=161901&section=2" alt="Album cover Always the Hard Way - Terror">
+            </div>
+            <div class="info pt-2">
+              <h5 class="title">Always the Hard Way Lorem ipsum dolor</h5>
+              <p class="artist pt-1">Terror</p>
+              <p class="year">2006</p>
+            </div>
+          </div>
+        </div>
       </div> <!-- / .grid-row -->
     </div> <!-- / .container -->
   </main>
