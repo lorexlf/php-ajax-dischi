e378521b0982c7627e6be8b5fe00951e07681202
Lorenzo <lorenzo.franzone@gmail.com>
2020-02-25 15:47:37 +0100

    Database

--- database.php
+++ database.php
@@ -5,61 +5,61 @@
       'title' => 'Always the Hard Way',
       'author' => 'Terror',
       'year' => 2006,
-      'poster' => ''
+      'poster' => 'https://fanart.tv/api/download.php?type=download&image=161901&section=2'
     ], // 001
     [
       'title' => 'Killswitch Engage',
-      'author' => 'Disarm the Discent',
-      'year' => 2013,
-      'poster' => ''
+      'author' => 'The End of the Heartache',
+      'year' => 2004,
+      'poster' => 'https://vignette.wikia.nocookie.net/ydgmusic/images/7/7a/KSEGTEOH.jpg/revision/latest?cb=20170128183523'
     ], // 002
     [
       'title' => 'Black Flame',
       'author' => 'Bury Tomorrow',
       'year' => 2018,
-      'poster' => ''
+      'poster' => 'https://wikitesti.com/wp-content/uploads/2019/10/black-flame-bury-tomorrow.jpg'
     ], // 003
     [
       'title' => 'The Jester Race',
       'author' => 'In Flames',
       'year' => 2002,
-      'poster' => ''
+      'poster' => 'https://lh3.googleusercontent.com/proxy/zP0mLmjJpuNGaElfMJcsATEFovpSsWb7ThkwGBtrWMPZX3pk2DNSObsvW-dDQo6x2M3kfnoVYSa_akVG_wLDawuMk_LGEv-VJ755tf9jT32w-aKKs7y9oOpmm-K2xln6sZ0uwvPgEA'
     ], // 004
     [
       'title' => 'The Rise of Brutality',
       'author' => 'Hatebreed',
       'year' => 2003,
-      'poster' => ''
+      'poster' => 'https://www.metalsucks.net/wp-content/uploads/2013/10/Hatebreed-The-Rise-of-Brutality-.jpg'
     ], // 005
     [
       'title' => 'Reign in Blood',
       'author' => 'Slayer',
       'year' => 1986,
-      'poster' => ''
+      'poster' => 'https://images.genius.com/cc91c005311c9e7d0bd9ef72d2f6f3d4.1000x1000x1.jpg'
     ], // 006
     [
       'title' => 'Peace Sells...But Who\'s Buying',
       'author' => 'Megadeth',
       'year' => 1986,
-      'poster' => ''
+      'poster' => 'https://images.genius.com/8e0bf7e3de19884439c40821adc58365.1000x1000x1.jpg'
     ], // 007
     [
       'title' => 'Iron Maiden',
       'author' => 'Somewhere in Time',
       'year' => 1986,
-      'poster' => ''
+      'poster' => 'https://i.pinimg.com/originals/6b/17/ec/6b17eca11a4195c73884f58d6440d6cf.jpg'
     ], // 008
     [
-      'title' => 'The Dissent of Man',
-      'author' => 'Bad Religion',
-      'year' => 2010,
-      'poster' => ''
+      'title' => 'Dream Evil',
+      'author' => 'Dio',
+      'year' => 1987,
+      'poster' => 'https://images.genius.com/6822ce146c77915a02ab59dbd0230f36.1000x1000x1.jpg'
     ], // 009
     [
-      'title' => 'Endgame',
-      'author' => 'Rise Against',
-      'year' => 2011,
-      'poster' => ''
+      'title' => 'Out of This World',
+      'author' => 'Europe',
+      'year' => 1988,
+      'poster' => 'https://studiodiscography.files.wordpress.com/2017/10/1988-8-out-of-this-world.jpeg'
     ], // 010
   ]; // $database
 
--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 </head>
 <body>
 
+  <!-- Header -->
   <header>
     <div class="container">
       <div class="grid-row">
@@ -25,6 +26,26 @@
       </div> <!-- / .grid-row -->
     </div> <!-- / .container -->
   </header>
+
+  <!-- Main -->
+  <main class="py-5 txt-small txt-center">
+    <div class="container">
+      <div class="grid-row">
+        <!-- Col-1 -->
+        <div class="col-sm-6 col-md-4">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div> <!-- / Col-1 -->
+        <!-- Col-2 -->
+        <div class="col-sm-6 col-md-4">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div> <!-- / Col-2 -->
+        <!-- Col-3 -->
+        <div class="col-sm-6 col-md-4">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div> <!-- / Col-3 -->
+      </div> <!-- / .grid-row -->
+    </div> <!-- / .container -->
+  </main>
 
 </body>
 </html>
